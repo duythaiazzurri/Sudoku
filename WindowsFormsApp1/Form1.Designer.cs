6d1f0835ba380a811ab3708b7be87e19e9d5ca01
duythaiazzurri <16521092@gm.uit.edu.vn>
2018-11-04 03:40:03 +0700

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace sudokuu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSolution = new System.Windows.Forms.Button();
+            this.ComboBox1 = new System.Windows.Forms.ComboBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSolution
+            // 
+            this.btnSolution.Location = new System.Drawing.Point(192, 309);
+            this.btnSolution.Name = "btnSolution";
+            this.btnSolution.Size = new System.Drawing.Size(75, 29);
+            this.btnSolution.TabIndex = 0;
+            this.btnSolution.Text = "Solution";
+            // 
+            // ComboBox1
+            // 
+            this.ComboBox1.FormattingEnabled = true;
+            this.ComboBox1.Items.AddRange(new object[] {
+            "Easy",
+            "Medium",
+            "Hard"});
+            this.ComboBox1.Location = new System.Drawing.Point(64, 10);
+            this.ComboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.ComboBox1.Name = "ComboBox1";
+            this.ComboBox1.Size = new System.Drawing.Size(255, 24);
+            this.ComboBox1.TabIndex = 8;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(12, 14);
+            this.Label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(46, 17);
+            this.Label1.TabIndex = 7;
+            this.Label1.Text = "Level:";
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(43, 309);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(102, 29);
+            this.btnNew.TabIndex = 9;
+            this.btnNew.Text = "New game";
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.AllowUserToAddRows = false;
+            this.DataGridView1.AllowUserToDeleteRows = false;
+            this.DataGridView1.AllowUserToOrderColumns = true;
+            this.DataGridView1.AllowUserToResizeColumns = false;
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.ColumnHeadersVisible = false;
+            this.DataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            this.DataGridView1.Location = new System.Drawing.Point(16, 43);
+            this.DataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.RowHeadersVisible = false;
+            this.DataGridView1.Size = new System.Drawing.Size(304, 247);
+            this.DataGridView1.TabIndex = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 25;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 25;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 25;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 25;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 25;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 25;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Column7";
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 25;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Column8";
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 25;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Column9";
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 350);
+            this.Controls.Add(this.btnSolution);
+            this.Controls.Add(this.ComboBox1);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.DataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sudoku";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btnSolution;
+        internal System.Windows.Forms.ComboBox ComboBox1;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Button btnNew;
+        internal System.Windows.Forms.DataGridView DataGridView1;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        internal System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sudokuu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+            btnNew.Click += btnNew_Click;
+            DataGridView1.Paint += DataGridView1_Paint;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataGridView1.Rows.Add(9);
+            ComboBox1.SelectedIndex = 0;
+            btnNew.PerformClick();
+        }
+        private void DataGridView1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(new Pen(Color.Black, 2), 75, 0, 75, 228);
+            e.Graphics.DrawLine(new Pen(Color.Black, 2), 150, 0, 150, 228);
+            e.Graphics.DrawLine(new Pen(Color.Black, 2), 0, 66, 228, 66);
+            e.Graphics.DrawLine(new Pen(Color.Black, 2), 0, 132, 228, 132);
+        }
+        
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
